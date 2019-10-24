f04484d7346206e2eefb49c6b141b72b69a64d7f
Terrance Li <tyli@berkeley.edu>
2019-10-23 23:15:19 -0700

    Attempt to fix 404 on refresh

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import Home from './components/Home';
@@ -15,24 +15,25 @@ import ProjectInfo from './components/Judge/ProjectInfo/ProjectInfo';
 import Spreadsheet from './components/Organizer/HackerSpreadsheet/Spreadsheet';
 import Winners from './components/Organizer/Winners/WinnerPage';
 
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path="/navigation" component={Navigation} />
-          <Route path="/data-entry" component={DataEntry} />
-          <Route path="/judge-info" component={JudgeInfo} />
-          <Route path="/overview" component={ScoringOverview} />
-          <Route path="/instructions" component={Instructions} />
-          <Route path="/judge-login" component={JudgeLogin} />
-          <Route path="/project-info" component={ProjectInfo} />
-          <Route path="/project-breakdown" component={ProjectBreakdown} />
-          <Route path="/hacker-spreadsheet" component={Spreadsheet} />
-          <Route path="/winners" component={Winners} />
-        </Switch>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/navigation" component={Navigation} />
+            <Route exact path="/data-entry" component={DataEntry} />
+            <Route exact path="/judge-info" component={JudgeInfo} />
+            <Route exact path="/overview" component={ScoringOverview} />
+            <Route exact path="/instructions" component={Instructions} />
+            <Route exact path="/judge-login" component={JudgeLogin} />
+            <Route exact path="/project-info" component={ProjectInfo} />
+            <Route exact path="/project-breakdown" component={ProjectBreakdown} />
+            <Route exact path="/hacker-spreadsheet" component={Spreadsheet} />
+            <Route exact path="/winners" component={Winners} />
+          </Switch>
+        </Router>
       </div>
     );
   }
